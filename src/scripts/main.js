2ac290848855e333e1eb9b3cbc3dbc5f20f60f68
rennan veras <rennanhenriqueneves@gmail.com>
2023-05-23 16:08:27 -0300

    criação do hero, e seções do site com plugin de fade lareal ao descer a pagina, criação de contagem regressiva feita

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,29 @@
-console.log('Ola')
+AOS.init();
+
+const dataDoEvento = new Date("Dec 07, 2023 19:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contasAsHoras = setInterval(function() {
+    const horaAtual = new Date();
+    const timeStampAtual = horaAtual.getTime();
+
+    const distanciaAteOEvento= timeStampDoEvento - timeStampAtual;
+
+    const diaEmMs = 1000 * 60 * 60 * 24;
+    const horasEmMs = 1000 * 60 * 60;
+    const minutosEmMs = 1000 * 60;
+
+    const diasAteOEvento = Math.floor(distanciaAteOEvento / diaEmMs);
+    const horasAteOEvento = Math.floor((distanciaAteOEvento % diaEmMs) / horasEmMs);
+    const minutosAteOEvento = Math.floor((distanciaAteOEvento % horasEmMs) / minutosEmMs);
+    const segundosAteOEvento = Math.floor((distanciaAteOEvento % minutosEmMs) / 1000);
+
+    const tempoFinal = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`
+
+    document.getElementById('contador').innerHTML = tempoFinal;
+    
+    if(distanciaAteOEvento < 0){
+        clearInterval(contasAsHoras);
+        document.getElementById('contador').innerHTML = `Evento expirado`
+    }
+}, 1000);
