a7c7e7d1a6c94958f74b9a0cb23c47c8a4454483
rennan veras <rennanhenriqueneves@gmail.com>
2023-11-24 12:59:29 -0300

    mudando a logo e adicionando o footer

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
 AOS.init();
 
-const dataDoEvento = new Date("Dec 07, 2023 19:00:00");
+const dataDoEvento = new Date("Feb 07, 2024 19:00:00");
 const timeStampDoEvento = dataDoEvento.getTime();
 
 const contasAsHoras = setInterval(function() {
